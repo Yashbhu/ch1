a4d70e16299523cd41686eb549b0c3f1c6641834
Yash Bahuguna <yashbahuguna918@gmail.com>
2025-06-15 22:16:46 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 //advance or modern
 import express from 'express';
 
-const app=express()
-const port = process.env.PORT||3000
-
-app.get('/',(req,res)=>{
+const app = express()
+const port = process.env.PORT || 3000//hardcorded port
+app.get('/', (req, res) => {
     res.send('hello')
 });
+const joke=[{
+  joke1: "Why don't scientists trust atoms? Because they make up everything!",
+  joke2: "Why did the function cross the road? To return to the other side!",
+  joke3: "I told my computer I needed a break, and now it won’t stop sending me KitKat ads."
+}]
 
-app.listen(port,()=>{
+app.get('/',(req,res)=>{
+    res.json(joke)
+})
+app.listen(port, () => {
     console.log(`port is listening to port ${port}`)
 })
- 
+
